a7df8b4b6f16e368da0068ea709a31894f743f9b
Witold Jas <jaswitold1@gmail.com>
2021-05-08 12:16:10 +0200

    problem with metro

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,42 @@
 import React from "react";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://chat.thewidlarzgroup.com/api/graphql",
+  cache: new InMemoryCache(),
+});
+
+const GET_MESSAGES = gql`
+  query userRooms {
+    user {
+      firstName
+    }
+  }
+`;
+
+const Messages = (user) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+  return JSON.stringify(data);
+};
+
 const ChatScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>Chat</Text>
-    </SafeAreaView>
+    <ApolloProvider>
+      <SafeAreaView>
+        <Messages user='Witold' />
+      </SafeAreaView>
+    </ApolloProvider>
   );
 };
 
