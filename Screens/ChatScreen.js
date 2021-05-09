1cac29e296382739871c9507c85868a6bacff7b9
Witold Jas <jaswitold1@gmail.com>
2021-05-09 15:06:32 +0200

    switched to phoenix/absinthe

--- Screens/ChatScreen.js
+++ Screens/ChatScreen.js
@@ -15,8 +15,17 @@ import {
   gql,
   createHttpLink,
   useMutation,
+  HttpLink,
+  split,
+  useSubscription,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+
+import * as AbsintheSocket from "@absinthe/socket";
+import { createAbsintheSocketLink } from "@absinthe/socket-apollo-link";
+import { Socket as PhoenixSocket } from "phoenix";
+import { hasSubscription } from "@jumpn/utils-graphql";
 
 const ChatScreen = (route) => {
   console.log(route.navigation.state.params.id);
@@ -25,10 +34,9 @@ const ChatScreen = (route) => {
   });
 
   const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
     const token =
       "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjaGF0bHkiLCJleHAiOjE2MjE1MDY3ODYsImlhdCI6MTYxOTA4NzU4NiwiaXNzIjoiY2hhdGx5IiwianRpIjoiMjIxZDExODUtOTI1NS00ZGVjLWJkMjYtYmYwZWNlZDc1MmMxIiwibmJmIjoxNjE5MDg3NTg1LCJzdWIiOiI0ZjIzNDY4Yi00ZGZkLTRkMmUtYTYwNC0wZjIwMjAxZTUyOGUiLCJ0eXAiOiJhY2Nlc3MifQ.a63UF8b_9IsXzPos54ZkZWohNnQd8aRbItvyDy8Dw507NNnjalHrvE3gvBQpKT029CDB8jyvO87F_FrgdZxHAg";
-    // return the headers to the context so httpLink can read them
+
     return {
       headers: {
         ...headers,
@@ -37,13 +45,40 @@ const ChatScreen = (route) => {
     };
   });
 
+  const authedHttpLink = authLink.concat(httpLink);
+
+  const phoenixSocket = new PhoenixSocket(
+    "wss://chat.thewidlarzgroup.com/socket",
+    {
+      params: () => {
+        return {
+          token:
+            "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjaGF0bHkiLCJleHAiOjE2MjE1MDY3ODYsImlhdCI6MTYxOTA4NzU4NiwiaXNzIjoiY2hhdGx5IiwianRpIjoiMjIxZDExODUtOTI1NS00ZGVjLWJkMjYtYmYwZWNlZDc1MmMxIiwibmJmIjoxNjE5MDg3NTg1LCJzdWIiOiI0ZjIzNDY4Yi00ZGZkLTRkMmUtYTYwNC0wZjIwMjAxZTUyOGUiLCJ0eXAiOiJhY2Nlc3MifQ.a63UF8b_9IsXzPos54ZkZWohNnQd8aRbItvyDy8Dw507NNnjalHrvE3gvBQpKT029CDB8jyvO87F_FrgdZxHAg",
+        };
+      },
+    }
+  );
+
+  const absintheSocket = AbsintheSocket.create(phoenixSocket);
+
+  const websocketLink = createAbsintheSocketLink(absintheSocket);
+
+  const link = split(
+    (operation) => hasSubscription(operation.query),
+    websocketLink,
+    authedHttpLink
+  );
+
+  const cache = new InMemoryCache();
+
   const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+    link,
+    cache,
   });
 
   const GET_MESSAGES = gql`
-    {
+    query x {
+   
       room(id: "${route.navigation.state.params.id}") {
         messages {
           body
@@ -63,6 +98,8 @@ const ChatScreen = (route) => {
       }
     }
   `;
+
+  //// pobieranie wiadomosci
   const Messages = () => {
     const { data } = useQuery(GET_MESSAGES);
 
@@ -90,7 +127,7 @@ const ChatScreen = (route) => {
   const MessageInput = () => {
     const [message, setMessage] = useState({});
     const [sendMessage] = useMutation(SEND_MESSAGES);
-    console.log(message);
+
     const handleInput = (text) => {
       setMessage(text);
     };
